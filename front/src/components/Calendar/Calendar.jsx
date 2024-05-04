5ec55ffcf7683bc407342b13b6491aaedbe062b6
x1agy <skizzzgrand@gmail.com>
2024-05-04 15:13:58 +0300

    asdasd

--- front/src/components/Calendar/Calendar.jsx
+++ front/src/components/Calendar/Calendar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Button, Calendar, Dropdown, Flex, Form, Input, Modal, Select, TimePicker } from 'antd';
+import { Button, Calendar, Col, Dropdown, Flex, Form, Input, Modal, Radio, Row, Select, TimePicker, Typography } from 'antd';
 
 import styles from './calendar.module.css';
 import { UserContext } from '../../App';
@@ -113,6 +113,73 @@ const CalendarComponent = () => {
         fullscreen={false} 
         className={styles.calendar} 
         fullCellRender={onCellRender}
+        headerRender={({ value, type, onChange, onTypeChange }) => {
+          const start = 0;
+          const end = 12;
+          const monthOptions = [];
+          let current = value.clone();
+          const localeData = value.localeData();
+          const months = [];
+          for (let i = 0; i < 12; i++) {
+            current = current.month(i);
+            months.push(localeData.monthsShort(current));
+          }
+          for (let i = start; i < end; i++) {
+            monthOptions.push(
+              <Select.Option key={i} value={i} className="month-item">
+                {months[i]}
+              </Select.Option>,
+            );
+          }
+          const year = value.year();
+          const month = value.month();
+          const options = [];
+          for (let i = year - 10; i < year + 10; i += 1) {
+            options.push(
+              <Select.Option key={i} value={i} className="year-item">
+                {i}
+              </Select.Option>,
+            );
+          }
+          return (
+            <div
+              style={{
+                padding: 8,
+                marginInlineStart: 'auto'
+              }}
+            >
+              <Row gutter={8}>
+                <Col>
+                  <Select
+                    size="small"
+                    dropdownMatchSelectWidth={false}
+                    className="my-year-select"
+                    value={year}
+                    onChange={(newYear) => {
+                      const now = value.clone().year(newYear);
+                      onChange(now);
+                    }}
+                  >
+                    {options}
+                  </Select>
+                </Col>
+                <Col>
+                  <Select
+                    size="small"
+                    dropdownMatchSelectWidth={false}
+                    value={month}
+                    onChange={(newMonth) => {
+                      const now = value.clone().month(newMonth);
+                      onChange(now);
+                    }}
+                  >
+                    {monthOptions}
+                  </Select>
+                </Col>
+              </Row>
+            </div>
+          );
+        }}
       />
       <Button 
         onClick={() => setIsModalOpen(true)} 
